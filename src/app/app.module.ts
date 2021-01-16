23c182639e0c36bc14ae27eab62b02ae6f5e33a5
scryer-niall <niall@scryer.io>
2021-01-16 17:55:41 +0000

    three template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './modules/app-routing/app-routing.module';
-import { AppComponent } from './ui/app-root/app-root';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {EngineComponent} from './engine/engine.component';
+import {UiInfobarBottomComponent} from './ui/ui-infobar-bottom/ui-infobar-bottom.component';
+import {UiInfobarTopComponent} from './ui/ui-infobar-top/ui-infobar-top.component';
+import {UiSidebarLeftComponent} from './ui/ui-sidebar-left/ui-sidebar-left.component';
+import {UiSidebarRightComponent} from './ui/ui-sidebar-right/ui-sidebar-right.component';
+import {UiComponent} from './ui/ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EngineComponent,
+    UiComponent,
+    UiInfobarBottomComponent,
+    UiInfobarTopComponent,
+    UiSidebarLeftComponent,
+    UiSidebarRightComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
